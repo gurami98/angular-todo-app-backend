f1beccfb2cfcd2a9d5e0759589a793f0b91d9bdc
gurami98 <chilindrishviligurami@gmail.com>
2022-09-18 02:36:13 +0400

    add backend for angular todo app with NgRx

---
+++ index.js
@@ -0,0 +1,14 @@
+const jsonServer = require('json-server');
+const auth = require('json-server-auth');
+const cors = require('cors');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const port = process.env.PORT || 3200;
+
+server.db = router.db
+
+server.use(cors())
+server.use(auth);
+server.use(router);
+
+server.listen(port);
